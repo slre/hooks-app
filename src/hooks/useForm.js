8d5cf5aec3895a523ccf138166d63da6b51e0344
Salvador <isc.slre@gmail.com>
2022-06-08 14:15:59 -0500

    ToDo ready, optimization pending...

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 
 export const useForm = (  initialState = {}) => {
     const [values, setValues] = useState( initialState );
@@ -7,9 +7,11 @@ export const useForm = (  initialState = {}) => {
             ...values,
             [target.name]:target.value
         });
-
+    }
+    const reset = () => {
+        setValues(initialState);
     }
 
-    return [ values,handleInputChange ]
+    return [ values,handleInputChange,reset ]
 
 }
